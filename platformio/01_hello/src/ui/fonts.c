5bd11f81a895d67eb260a292aae0b8d3395f6533
Andre Durao <andreluisdurao@gmail.com>
2024-12-20 15:48:00 -0300

    Remove font headers

--- platformio/01_hello/src/ui/fonts.c
+++
@@ -1,2 +0,0 @@
-// fonts.c
-// #include "beta2.c"
--- platformio/01_hello/src/ui/fonts.h
+++
@@ -1,9 +0,0 @@
-// fonts.h
-#include "lvgl.h"
-
-#ifndef FONTS_H
-#define FONTS_H
-
-// extern const lv_font_t beta2;  // Declare the font here
-
-#endif // FONTS_H
